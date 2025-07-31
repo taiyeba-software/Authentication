549604a2c906eca92c16c273f56fbf3062e03105
taiyeba-software <islamtaiyeba38@gmail.com>
2025-07-31 16:55:41 +0600

    jwt implemented

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 const express = require('express');
-
 const  bcrypt =require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
+// making the token
+const payload = {
+  email: "taiyeba@rajkonna.com",
+  userId: "64ba12x7z"
+};
+
+const secretKey = "yourSecretRajkonna";
+
+const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });
+
+console.log("Token:", token);
+
+
+
+//jwt.verify : server checks if the token is valid
+const decoded = jwt.verify(token, secretKey);
+console.log("Decoded Info:", decoded);
+
+//only decode the token , don't checks the security , just to see the payload
+const info = jwt.decode(token);
+console.log("Decoded Info Without Verify:", info);
 
 
 app.get('/', (req, res) => {
-  res.end('Cookie has been removed!');
+  res.send(` Your token is: ${token}`);
 });
 
-app.get('/read', (req, res) => {
-  
-  res.end('read page!');
-});
-
-
-app.post('/reginter', async(req,res)=>{
-   const {username,password}=req.body;
-
-   const saltRounds = 10;
-   const hashedpassword = await bcrypt.hash(password, saltRounds);
-   // Here you would typically save the username and hashed password to your database
-})
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
-// Export the app for testing purposes
