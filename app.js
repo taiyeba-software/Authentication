ef4319da27a153f6e93f097627008123e152aa18
taiyeba-software <islamtaiyeba38@gmail.com>
2025-07-31 13:36:28 +0600

    basic bcrypt

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
+
+const  bcrypt =require("bcrypt");
 
 const app = express();
 
-app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
-  res.cookie('name', "harshita");
-  res.end('Cookie has been set!');
+  res.end('Cookie has been removed!');
 });
 
 app.get('/read', (req, res) => {
   
   res.end('read page!');
 });
+
+
+app.post('/reginter', async(req,res)=>{
+   const {username,password}=req.body;
+
+   const saltRounds = 10;
+   const hashedpassword = await bcrypt.hash(password, saltRounds);
+   // Here you would typically save the username and hashed password to your database
+})
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
