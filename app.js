eeb5f2ef31051f865bc062d9205488a529114e9c
taiyeba-software <islamtaiyeba38@gmail.com>
2025-07-31 22:55:07 +0600

    setup of tutorial 15

--- app.js
+++ app.js
@@ -1,37 +1,20 @@
-const express = require('express');
-const  bcrypt =require("bcrypt");
-const jwt = require('jsonwebtoken');
-
+const express =require('express');
 const app = express();
 
-// making the token
-const payload = {
-  email: "taiyeba@rajkonna.com",
-  userId: "64ba12x7z"
-};
+const path = require('path');
+const cookieParser = require('cookie-parser');
 
-const secretKey = "yourSecretRajkonna";
-
-const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });
-
-console.log("Token:", token);
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies from HTML forms
+app.use(express.static(path.join(__dirname, 'public')));// Serve static files from the 'public' directory
+app.use(cookieParser());// Parse cookies from the request headers
 
 
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+})
 
-//jwt.verify : server checks if the token is valid
-const decoded = jwt.verify(token, secretKey);
-console.log("Decoded Info:", decoded);
-
-//only decode the token , don't checks the security , just to see the payload
-const info = jwt.decode(token);
-console.log("Decoded Info Without Verify:", info);
-
-
-app.get('/', (req, res) => {
-  res.send(` Your token is: ${token}`);
-});
-
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})
