4318f8aaf41a7f5344305b4d2899a65efbf0289e
taiyeba-software <islamtaiyeba38@gmail.com>
2025-08-01 15:30:27 +0600

    mongo db connection error

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express =require('express');
 const app = express();
+const usermodel = require('./models/user'); // Import the user model
+require('dotenv').config();
+
 
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const user = require('./models/user');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -12,8 +16,20 @@ app.use(cookieParser());// Parse cookies from the request headers
 
 
 app.get('/',(req,res)=>{
-    res.send('Hello World!');
+    res.render('index');
 })
+
+app.post('/create', async(req,res)=>{
+     let { username, password, email, age } = req.body; // Extract data from the request body
+     // Create a new user document using the user model
+    let createduser = await usermodel.create({
+         username,
+         password,
+        email,
+        age
+    })
+    res.send('User created successfully'); // Send a response back to the client
+});
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose =require('mongoose');
+require('dotenv').config();
+// Connect to MongoDB using Mongoose
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected ✅"))
+.catch((err) => console.log("MongoDB Connection Error ❌", err));
+
+const userSchema = new mongoose.Schema({
+    username:String,
+    password:String,
+    email:String,
+    age:Number
+
+});
+
+module.exports = mongoose.model('User', userSchema);
+// This code defines a Mongoose schema for a User model with fields for username, password, email, and age.
+// It also connects to a MongoDB database running on localhost at port 27017 with the database name 'database_name'.    
